bf5b2ea6ebae2eafd0a2d1d65a67bce9cf5cb492
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2025-03-26 23:18:36 -0400

    gideon update

---
+++ gideon-gomm/content.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  fetch(`https://api.are.na/v2/channels/will-gideon-gomm?per=100`, {cache: 'no-store'})
+  .then(response => response.json())
+  .then(data => { viewJson(data.contents); });
+});
+  
+function viewJson(blocks){
+  console.log(blocks)
+  blocks.forEach((block) => {
+    if (block.image && block.image.original.url) {
+      let imgTitle = block.title.split('.')[0];
+      let img = document.getElementById(imgTitle);
+      img.src = block.image.original.url;
+    }
+  });
+}
